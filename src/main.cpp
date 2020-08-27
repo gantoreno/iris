2a3db302d42e69efb56a1ff7860108db67e004ba
Gabriel Moreno <gamoreno@urbe.edu.ve>
2020-08-27 13:17:04 -0400

    Revert stash and implement Layer matrix methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include "../include/iris/iris.hpp"
+#include "../include/iris.hpp"
 
 using namespace std;
+using namespace iris;
 
 int main(int argc, char **argv) {
-    iris::Network inet = iris::Network({3, 2, 3});
-    
-    inet.describe();
+    Network *n = new Network({3, 2, 3});
+
+    n->setInput({1, 0, 2});
+    n->describe();
 
     return 0;
 }
